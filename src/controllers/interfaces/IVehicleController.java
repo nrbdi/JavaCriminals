7151c3dab493c77ee4cb627636d6f292c0881fdd
RendersC <crewsrenders@gmail.com>
2025-01-29 18:15:54 +0500

    catalog + characteristic of cars

--- src/controllers/interfaces/IVehicleController.java
+++
@@ -1,9 +0,0 @@
-package controllers.interfaces;
-
-public interface IVehicleController {
-    String getAllVehicles();
-    String getVehiclesByType(String vehicleType);
-    String getVehiclesByBrand(String brand);
-    String getVehicleById(int id);
-    boolean updateVehicleStatus(int id, String status);
-}
